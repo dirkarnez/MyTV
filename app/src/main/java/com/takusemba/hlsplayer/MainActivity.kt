2b42c37c4a7a8e26540a23ad0c12eabced54d7b5
dirkarnez <smalldirkalex@gmail.com>
2021-05-26 16:57:06 +0800

    - add AdaptiveMediaSourceEventListener, onKeyDown

--- app/src/main/java/com/takusemba/hlsplayer/MainActivity.kt
+++ app/src/main/java/com/takusemba/hlsplayer/MainActivity.kt
@@ -4,20 +4,18 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
+import android.view.KeyEvent
 import android.widget.Toast
-import com.google.android.exoplayer2.DefaultLoadControl
-import com.google.android.exoplayer2.DefaultRenderersFactory
-import com.google.android.exoplayer2.ExoPlayerFactory
-import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.source.AdaptiveMediaSourceEventListener
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView
+import com.google.android.exoplayer2.upstream.DataSpec
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
-import java.lang.Exception
 import java.util.*
 
 
@@ -32,6 +30,25 @@ class MainActivity : AppCompatActivity() {
             .build()
 
     private var player: SimpleExoPlayer? = null
+
+    /**
+     * If you handled the event, return true.
+     * If you want to allow the event to be handled by the next receiver, return false.
+     */
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            stop()
+            finish();
+            return true;
+        }
+
+        if (keyCode >= KeyEvent.KEYCODE_1 && keyCode <= KeyEvent.KEYCODE_9) {
+            Toast.makeText(this@MainActivity, String.format("KeyDown %d", keyCode),Toast.LENGTH_SHORT).show()
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,22 +93,45 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        timerObj.schedule(timerTaskObj, 0, 1000 * 10)
         //timerObj.schedule(timerTaskObj, 0, 1000 * 60 * 15)
-        timerObj.schedule(timerTaskObj, 0, 1000 * 60 * 15)
     }
 
     private fun play(urlString : String) {
         player = ExoPlayerFactory.newSimpleInstance(
                 DefaultRenderersFactory(this), DefaultTrackSelector(), DefaultLoadControl())
         val playerView = findViewById<SimpleExoPlayerView>(R.id.player_view)
-        //val uri = Uri.parse(BuildConfig.STREAMING_URL)
 
         val uri = Uri.parse(urlString)
 
         playerView.player = player
 
         val dataSourceFactory = DefaultDataSourceFactory(this, "user-agent")
-        val mediaSource = HlsMediaSource(uri, dataSourceFactory, handler, null)
+        val mediaSource = HlsMediaSource(uri, dataSourceFactory, handler, object : AdaptiveMediaSourceEventListener {
+            override fun onLoadStarted(dataSpec: DataSpec?, dataType: Int, trackType: Int, trackFormat: Format?, trackSelectionReason: Int, trackSelectionData: Any?, mediaStartTimeMs: Long, mediaEndTimeMs: Long, elapsedRealtimeMs: Long) {
+                Toast.makeText(this@MainActivity, "onLoadStarted", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onLoadCompleted(dataSpec: DataSpec?, dataType: Int, trackType: Int, trackFormat: Format?, trackSelectionReason: Int, trackSelectionData: Any?, mediaStartTimeMs: Long, mediaEndTimeMs: Long, elapsedRealtimeMs: Long, loadDurationMs: Long, bytesLoaded: Long) {
+                Toast.makeText(this@MainActivity, "onLoadCompleted", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onLoadCanceled(dataSpec: DataSpec?, dataType: Int, trackType: Int, trackFormat: Format?, trackSelectionReason: Int, trackSelectionData: Any?, mediaStartTimeMs: Long, mediaEndTimeMs: Long, elapsedRealtimeMs: Long, loadDurationMs: Long, bytesLoaded: Long) {
+                Toast.makeText(this@MainActivity, "onLoadCanceled", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onLoadError(dataSpec: DataSpec?, dataType: Int, trackType: Int, trackFormat: Format?, trackSelectionReason: Int, trackSelectionData: Any?, mediaStartTimeMs: Long, mediaEndTimeMs: Long, elapsedRealtimeMs: Long, loadDurationMs: Long, bytesLoaded: Long, error: IOException?, wasCanceled: Boolean) {
+                Toast.makeText(this@MainActivity, "onLoadError", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onUpstreamDiscarded(trackType: Int, mediaStartTimeMs: Long, mediaEndTimeMs: Long) {
+                Toast.makeText(this@MainActivity, "onUpstreamDiscarded", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDownstreamFormatChanged(trackType: Int, trackFormat: Format?, trackSelectionReason: Int, trackSelectionData: Any?, mediaTimeMs: Long) {
+                Toast.makeText(this@MainActivity, "onDownstreamFormatChanged", Toast.LENGTH_SHORT).show()
+            }
+        })
 
         player?.prepare(mediaSource)
         player?.playWhenReady = true
